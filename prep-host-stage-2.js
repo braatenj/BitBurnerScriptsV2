763884fd5ec02c2a231479f29bd4799ff98e5994
Jacob Braaten <braatenj@gmail.com>
2024-04-15 14:16:36 -0500

    fixed missing ns reference

--- prep-host-stage-2.js
+++ prep-host-stage-2.js
@@ -4,7 +4,7 @@ export async function main(ns) {
     let hostMinSecurity = ns.getServerMinSecurityLevel(host);
     let hostMaxMoney = ns.getServerMaxMoney(host);
 
-    while((ns.getServerSecurityLevel(host) > hostMinSecurity) && (getServerMoneyAvailable(host) < hostMaxMoney)) {
+    while((ns.getServerSecurityLevel(host) > hostMinSecurity) && (ns.getServerMoneyAvailable(host) < hostMaxMoney)) {
         if(ns.getServerSecurityLevel > hostMinSecurity) {
             await ns.weaken(host);
         } else {
