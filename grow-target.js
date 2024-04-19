6a89c5bc0214152ed692a3e68561553e4d92e597
Jacob Braaten <braatenj@gmail.com>
2024-04-19 08:16:07 -0500

    reorganized and created a new BN1 daemon

--- grow-target.js
+++ grow-target.js
@@ -2,11 +2,11 @@ export async function main(ns) {
     let delay = 0;
     let target;
 
-    if(ns.args.length > 0) {
+    if(ns.args.length == 2) {
         target = ns.args[0];
         delay = ns.args[1];
     }
 
-    await ns.sleep(delay);
-    await ns.grow(target);
+
+    await ns.grow(target, {additionalMSec: delay});
 }
--- hacking-manager.js
+++ hacking-manager.js
@@ -11,10 +11,10 @@ export async function main(ns) {
         for(let i = 0; i < targets.length; i++) {
             let target = targets[i];
             target.getRootAccess();
-            prepServer(target);
+            await prepServer(target);
         }
 
-
+        let bestTarget = retrieveBestHackTarget(ns, servers);
 
         await ns.sleep(ONE_MINUTE);
     }
@@ -31,5 +31,9 @@ async function prepServer(server) {
     await ns.scp("prep-host-stage-1.js", server.name, "home");
     await ns.scp("prep-host-stage-2.js", server.name, "home");
     ns.exec("prep-host-stage-1.js", server.name);
+}
+
+function retrieveBestHackTarget(ns, servers) {
+    
 }
 
