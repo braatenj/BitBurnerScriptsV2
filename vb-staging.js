1f8fa04f8e6bfe10021ddce9ca2b6f9a116ce937
Jacob Braaten <braatenj@gmail.com>
2025-09-18 18:46:42 -0500

    staging had typo for checking bitnode

--- vb-staging.js
+++ vb-staging.js
@@ -10,7 +10,7 @@ export async function main(ns) {
   ns.disableLog(`ALL`);
   ns.print(`Initializing Vladburner fully automated scripts...`);
 
-  if (getCurrentBitnode(ns) == 1 && ns.getServer("home").maxRam() > 32) {
+  if (getCurrentBitnode(ns) == 1 && ns.getServer("home").maxRam > 32) {
     ns.spawn("BN1/daemon-advanced.js", { threads: 1, spawnDelay: 1500 });
   }
 
